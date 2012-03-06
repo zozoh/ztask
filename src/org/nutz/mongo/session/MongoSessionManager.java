c43e16bb5759bd0b8983c4788c35fcd240314083
Peter Zhang <zozohtnt@gmail.com>
2012-03-06 08:16:37 +0800

    加个通知锁，以便让MongonSession清除线程停的快点

--- src/org/nutz/mongo/session/MongoSessionManager.java
+++ src/org/nutz/mongo/session/MongoSessionManager.java
@@ -24,14 +24,21 @@ import com.mongodb.DBObject;
 
 /**
  * MongoSession会话管理器,负责查询/生成MongoSession
+ * 
  * @author wendal(wendal1985@gmail.com)
- *
+ * 
  */
 public class MongoSessionManager implements SessionProvider {
 
 	private static final Log log = Logs.get();
+
 	private ManagerContext context;
+
 	private Thread cleaner;
+
+	private boolean stop;
+
+	private Object lock;
 
 	public MongoSessionManager(final MongoDao dao) {
 		this(dao, "http.session");
@@ -42,27 +49,42 @@ public class MongoSessionManager implements SessionProvider {
 		context.setMongoDao(dao);
 		context.setSessions(dao.getDB().getCollection(colName));
 		context.setProvider(new SessionValueAdpter());
+		lock = new Object();
 		DBCollection systemJs = dao.getDB().getCollection("system.js");
 		systemJs.remove(new BasicDBObject("_id", "mongoSessionClean"));
-		systemJs.insert(new BasicDBObject("_id", "mongoSessionClean").append("value", new Code(Files.read(Files.findFile("org/nutz/mongo/session/mongoSessionClean.js")))));
+		systemJs.insert(new BasicDBObject("_id", "mongoSessionClean").append(	"value",
+																				new Code(Files.read(Files.findFile("org/nutz/mongo/session/mongoSessionClean.js")))));
 		cleaner = new Thread("MongoSessionCleaner") {
 			public void run() {
-				while (true) {
+				while (!stop) {
 					try {
 						context.getMongoDao().getDB().eval("mongoSessionClean()");
-					} catch (Throwable e) {
+					}
+					catch (Throwable e) {
 						if (log.isWarnEnabled())
 							log.warn("Clean case some error", e);
 					}
-					try {
-						Thread.sleep(60000);
-					} catch (InterruptedException e) {
-						break;
-					} 
+
+					synchronized (lock) {
+						try {
+							lock.wait(60000);
+						}
+						catch (InterruptedException e) {
+							break;
+						}
+					}
 				}
 			}
 		};
 		cleaner.start();
+	}
+
+	@Override
+	public void notifyStop() {
+		stop = true;
+		synchronized (lock) {
+			lock.notifyAll();
+		}
 	}
 
 	public void setProvider(SessionValueAdpter provider) {
@@ -70,8 +92,9 @@ public class MongoSessionManager implements SessionProvider {
 	}
 
 	public MongoSession getSession(String key) {
-		DBObject dbo = context.getSessions().findOne(new BasicDBObject("_id", new ObjectId(
-				key)), new BasicDBObject("_id", 1));
+		DBObject dbo = context.getSessions()
+								.findOne(	new BasicDBObject("_id", new ObjectId(key)),
+											new BasicDBObject("_id", 1));
 		if (dbo == null || dbo.get("_id") == null)
 			return null;
 		return new MongoSession(context, (ObjectId) dbo.get("_id"));
@@ -92,10 +115,8 @@ public class MongoSessionManager implements SessionProvider {
 		if (!Strings.isBlank(key)) {
 			session = getSession(key);
 			if (session != null
-					&& req.getRemoteAddr().equals(
-							session.getValue("remoteAddr"))
-					&& req.getHeader("User-Agent").equals(
-							session.getValue("userAgent"))) {
+				&& req.getRemoteAddr().equals(session.getValue("remoteAddr"))
+				&& req.getHeader("User-Agent").equals(session.getValue("userAgent"))) {
 				session.touch();
 				return session;
 			}
@@ -110,11 +131,13 @@ public class MongoSessionManager implements SessionProvider {
 		return session;
 	}
 
-	public MongoHttpSession getHttpSession(HttpServletRequest req,
-			ServletContext servletContext, boolean createNew) {
+	public MongoHttpSession getHttpSession(	HttpServletRequest req,
+											ServletContext servletContext,
+											boolean createNew) {
 		MongoSession session = getSession(req, createNew);
 		if (session != null) {
-			MongoHttpSession httpSession = new MongoHttpSession(context,new ObjectId(session.getId()));
+			MongoHttpSession httpSession = new MongoHttpSession(context,
+																new ObjectId(session.getId()));
 			if (servletContext == null)
 				servletContext = req.getSession().getServletContext();
 			httpSession.setServletContext(servletContext);
@@ -129,7 +152,7 @@ public class MongoSessionManager implements SessionProvider {
 	}
 
 	public HttpSession getHttpSession(HttpServletRequest req, boolean createNew) {
-		MongoHttpSession session = getHttpSession(req,Mvcs.getServletContext(), createNew);
+		MongoHttpSession session = getHttpSession(req, Mvcs.getServletContext(), createNew);
 		if (session != null && session.isNew()) {
 			boolean flag = true;
 			if (((HttpServletRequest) req).getCookies() != null)
